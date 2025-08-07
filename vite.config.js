83090aba22c849cf9b9fe9eb3458b916cecce9bc
Azizbek <azizbeksharipov@gmail.com>
2025-08-07 13:26:03 +0500

    Fixed JSON and vite config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import viteConfigPaths from 'vite-tsconfig-paths';
 export default defineConfig({
   server: {
     port: 3010,
-    historyApiFallback: true, // ⚠️ Bu qator muhim!
+    historyApiFallback: true,
   },
   build: {
     reportCompressedSize: false,
